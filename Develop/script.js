6db5ef1190c0d10dc2bfb583aaef93eb98beed56
drewmckinney23 <drewmckinney23@gmail.com>
2022-05-17 15:24:12 -0400

    simplifying js for the time blocks

--- Develop/script.js
+++ Develop/script.js
@@ -19,18 +19,12 @@ $(document).ready(function () {
             var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
             if (blockTime < timeNow) {
-                $(this).removeClass("future");
-                $(this).removeClass("present");
                 $(this).addClass("past");
             }
             else if (blockTime === timeNow) {
-                $(this).removeClass("past");
-                $(this).removeClass("future");
                 $(this).addClass("present");
             }
             else {
-                $(this).removeClass("present");
-                $(this).removeClass("past");
                 $(this).addClass("future");
 
             }
@@ -47,6 +41,7 @@ $(document).ready(function () {
     $("#hour15 .description").val(localStorage.getItem("hour15"));
     $("#hour16 .description").val(localStorage.getItem("hour16"));
     $("#hour17 .description").val(localStorage.getItem("hour17"));
+
 
     timeTracker();
 })
